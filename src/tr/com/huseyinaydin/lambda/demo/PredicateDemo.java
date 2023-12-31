0428cef5d44aca31be76f43097f49815fe6a5270
Hüseyin Aydın <huseyinaydin99@gmail.com>
2023-12-31 12:20:13 +0300

    Update PredicateDemo.java

--- src/tr/com/huseyinaydin/lambda/demo/PredicateDemo.java
+++ src/tr/com/huseyinaydin/lambda/demo/PredicateDemo.java
@@ -17,6 +17,6 @@ public class PredicateDemo {
 
 		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
 
-		list1.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("print  Even: " + t));
+		list1.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("Çift: " + t));
 	}
 }

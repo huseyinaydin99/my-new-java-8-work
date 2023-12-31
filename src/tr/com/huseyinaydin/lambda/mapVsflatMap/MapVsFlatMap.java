57d0c8ac787e5dfa592f02629c6817d61f62a642
Hüseyin Aydın <huseyinaydin99@gmail.com>
2023-12-31 12:16:48 +0300

    Update MapVsFlatMap.java

--- src/tr/com/huseyinaydin/lambda/mapVsflatMap/MapVsFlatMap.java
+++ src/tr/com/huseyinaydin/lambda/mapVsflatMap/MapVsFlatMap.java
@@ -16,24 +16,24 @@ public class MapVsFlatMap {
 
         List<Customer> customers = EkartDataBase.getAll();
 
-        //List<Customer>  convert List<String> -> Data Transformation
+        //List<Customer>  convert List<String> -> Veri dönüşümü müşteri listesinden String listesine
         //mapping : customer -> customer.getEmail()
-        //customer -> customer.getEmail()  one to one mapping
+        //customer -> customer.getEmail()  one to one mapping(bire bir eşleme)
         List<String> emails = customers.stream()
                 .map(customer -> customer.getEmail())
                 .collect(Collectors.toList());
         System.out.println(emails);
 
-//customer -> customer.getPhoneNumbers()  ->> one to many mapping
-        //customer -> customer.getPhoneNumbers()  ->> one to many mapping
+//customer -> customer.getPhoneNumbers()  ->> one to many mapping(bire çok eşleme)
+        //customer -> customer.getPhoneNumbers()  ->> one to many mapping(bire çok eşleme)
         List<List<String>> phoneNumbers = customers.
                 stream().map(customer -> customer.getPhoneNumbers())
                 .collect(Collectors.toList());
         System.out.println(phoneNumbers);
 
-        //List<Customer>  convert List<String> -> Data Transformation
+        //List<Customer>  convert List<String> -> Veri dönüşümü müşteri listesinden String listesine
         //mapping : customer -> phone Numbers
-        //customer -> customer.getPhoneNumbers()  ->> one to many mapping
+        //customer -> customer.getPhoneNumbers()  ->> one to many mapping(bire çok eşleme)
         List<String> phones = customers.stream()
                 .flatMap(customer -> customer.getPhoneNumbers().stream())
                 .collect(Collectors.toList());
